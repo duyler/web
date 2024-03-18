3143e948663c2165533b566568276e57c5fea93a
Mikhail Ilinsky <mail@milinsky.ru>
2024-03-18 16:14:10 +1000

    Add resolving not found route

---
+++ src/State/ResolveRouteStateHandler.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Duyler\Web\State;
+
+use Duyler\EventBus\Contract\State\MainAfterStateHandlerInterface;
+use Duyler\EventBus\State\Service\StateMainAfterService;
+use Duyler\EventBus\State\StateContext;
+use Duyler\Http\Exception\NotFoundHttpException;
+use Duyler\Router\CurrentRoute;
+use Override;
+
+class ResolveRouteStateHandler implements MainAfterStateHandlerInterface
+{
+    #[Override]
+    public function handle(StateMainAfterService $stateService, StateContext $context): void
+    {
+        /** @var CurrentRoute $route */
+        $route = $stateService->getResultData();
+
+        if (false === $route->status) {
+            throw new NotFoundHttpException();
+        }
+    }
+
+    #[Override]
+    public function observed(StateContext $context): array
+    {
+        return ['Http.StartRouting'];
+    }
+}
