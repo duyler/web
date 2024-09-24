351e716298b2fbfd17115a8e5260dba45b06446f
Mikhail Ilinsky <milinsky@MacBook-Pro--Milinsky.local>
2024-09-25 00:18:48 +1000

    Fix naming

--- src/State/Action/ResultToResponseStateHandler.php
+++ src/State/Action/ResultToResponseStateHandler.php
@@ -9,7 +9,7 @@ use Duyler\EventBus\Dto\Event;
 use Duyler\EventBus\State\Service\StateMainEmptyService;
 use Duyler\EventBus\State\StateContext;
 use Duyler\Http\Event\Response;
-use Duyler\TwigWrapper\TwigWrapper;
+use Duyler\Web\Renderer\RendererInterface;
 use Duyler\Web\ViewCollection;
 use HttpSoft\Response\HtmlResponse;
 use HttpSoft\Response\JsonResponse;
@@ -22,7 +22,7 @@ class ResultToResponseStateHandler implements MainEmptyStateHandlerInterface
 {
     public function __construct(
         private ViewCollection $viewCollection,
-        private TwigWrapper $twigWrapper,
+        private RendererInterface $renderer,
     ) {}
 
     #[Override]
@@ -51,11 +51,11 @@ class ResultToResponseStateHandler implements MainEmptyStateHandlerInterface
             $actionView = $this->viewCollection->get($actionId);
 
             $template = str_replace('.', DIRECTORY_SEPARATOR, $actionView->viewName);
-            if (false === $this->twigWrapper->exists($template)) {
+            if (false === $this->renderer->exists($template)) {
                 throw new InvalidArgumentException("Template {$template} not found");
             }
 
-            $content = $this->twigWrapper->render($template);
+            $content = $this->renderer->render($template);
             $stateService->dispatchEvent(
                 new Event(
                     id: Response::ResponseCreated,
